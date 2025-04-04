0ec553541042fd73b7a4d000091be0948612cd84
BADNAM XD <yadavxd746@gmail.com>
2025-04-04 14:51:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHIVANSH474")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MISS_MAYA_NAGRI")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MISS_MAYA_NAGRI")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
