191101a7a94ed22273198642444a116e3e1be950
BADNAM XD <yadavxd746@gmail.com>
2025-04-04 21:10:45 +0530

    Update wishcute.py

--- SHUKLAMUSIC/plugins/Yumi/wishcute.py
+++ SHUKLAMUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SHUKLAMUSIC import app 
 
-SUPPORT_CHAT = "MASTIWITHFRIENDSXD"
+SUPPORT_CHAT = "TEAM_BADNAM_BOTS"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
